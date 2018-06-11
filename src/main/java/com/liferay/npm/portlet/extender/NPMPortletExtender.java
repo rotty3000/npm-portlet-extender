015fc5f4c9fd9e440bb70824eca962b28525d688
Jesse Rao <jesse.rao@liferay.com>
2018-06-11 18:07:52 -0400

    Use bundle wiring and bundle requirements API to determine opt-in, not bundle headers

--- src/main/java/com/liferay/npm/portlet/extender/NPMPortletExtender.java
+++ src/main/java/com/liferay/npm/portlet/extender/NPMPortletExtender.java
@@ -1,60 +1,85 @@
 package com.liferay.npm.portlet.extender;
 
-import java.util.Dictionary;
-import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
 
 import javax.portlet.Portlet;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.wiring.BundleCapability;
+import org.osgi.framework.wiring.BundleRequirement;
+import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.BundleWiring;
+import org.osgi.namespace.extender.ExtenderNamespace;
 import org.osgi.util.tracker.BundleTracker;
 import org.osgi.util.tracker.BundleTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NPMPortletExtender extends BundleTracker<ServiceRegistration<Portlet>> implements BundleActivator {
+public class NPMPortletExtender implements BundleActivator {
 
-	public NPMPortletExtender(
-			BundleContext context, int stateMask,
-			BundleTrackerCustomizer<ServiceRegistration<Portlet>> customizer) {
+	@Override
+	public void start(BundleContext context) throws Exception {
+		_bundleTracker = new BundleTracker<>(context, Bundle.ACTIVE,
+				new BundleTrackerCustomizer<ServiceRegistration<Portlet>>() {
 
-		super(context, stateMask, customizer);
+					@Override
+					public ServiceRegistration<Portlet> addingBundle(Bundle bundle, BundleEvent event) {
+						if (_optIn(bundle)) {
+							System.out.println("Found bundle with opt-in: " + bundle.getSymbolicName());
+							
+							// read package.json metadata
+							
+							// register portlet service
+						}
+
+						return null;
+					}
+
+					@Override
+					public void modifiedBundle(Bundle bundle, BundleEvent event, ServiceRegistration<Portlet> registration) {
+
+					}
+
+					@Override
+					public void removedBundle(Bundle bundle, BundleEvent event, ServiceRegistration<Portlet> registration) {
+						if (registration != null) {
+							registration.unregister();
+						}
+					}
+
+				});
+		
+		_bundleTracker.open();
 	}
 	
 	@Override
-	public void start(BundleContext context) throws Exception {
-		open();
+	public void stop(BundleContext context) throws Exception {
+		_bundleTracker.close();
+	}
+
+	private boolean _optIn(Bundle bundle) {
+		BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
 		
-		Bundle[] bundles = getBundles();
+		List<BundleWire> bundleWires = bundleWiring.getRequiredWires(ExtenderNamespace.EXTENDER_NAMESPACE);
 		
-		for (Bundle bundle : bundles) {
-			Dictionary<String, String> headers = bundle.getHeaders();
+		for (BundleWire bundleWire : bundleWires) {
+			BundleCapability bundleCapability = bundleWire.getCapability();
 			
-			Enumeration<String> keys = headers.keys();
+			Map<String, Object> attributes = bundleCapability.getAttributes();
 			
-			while(keys.hasMoreElements()) {
-				String key = keys.nextElement();
-				
-				if (key.equals("Require-Capability")) {
-					String value = headers.get(key);
-					
-					if (value.contains("osgi.extender;filter:=\"(osgi.extender=liferay.npm.portlet)")) {
-						NPMPortletMetadata metadata = _getMetaData();
-						
-						System.out.println("Registering NPM portlet...");
-						
-						// Register portlet service using metadata
-						
-						break;
-					}
-				}
+			Object value = attributes.get(ExtenderNamespace.EXTENDER_NAMESPACE);
+			
+			if ((value != null) && value.equals("liferay.npm.portlet")) {
+				return true;
 			}
-			
-			// p = createPortlet(md)
-			// registerPortlet(p, md)
 		}
+		
+		return false;
 	}
 
 	private NPMPortletMetadata _getMetaData() {
@@ -62,11 +87,8 @@ public class NPMPortletExtender extends BundleTracker<ServiceRegistration<Portle
 		return null;
 	}
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		close();
-	}
-
 	private static final Logger _logger = LoggerFactory.getLogger(NPMPortletExtender.class);
+
+	private BundleTracker<ServiceRegistration<Portlet>> _bundleTracker;
 
 }
